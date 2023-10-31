5ad33b95c610622111c13cd4c618569cb6b44559
Pritam Kulkarni <pritam1984314@gmail.com>
2023-10-31 07:55:55 +0530

    adding notebook and stage 1 code

---
+++ src/ner_pipelines/components/data_ingestion.py
@@ -0,0 +1,22 @@
+import os
+import urllib.request as request
+import zipfile
+from ner_pipelines import logger
+from ner_pipelines.config import DataIngestionConfig
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+
+    
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            logger.info(self.config.source_URL)
+            filename, headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename = self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists")
